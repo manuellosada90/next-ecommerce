b8315dc2d4e3001668406b7cee6db571de9ae2b3
manuel losada <manuellosada90@gmail.com>
2023-01-31 13:50:22 -0300

    server changes

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,10 +1,14 @@
 export async function getItems() {
-  const request = await fetch("http://localhost:3000/api/items");
+  const request = await fetch(
+    "https://mocki.io/v1/62342ece-d822-46e8-8664-b6338ea301cb"
+  );
   const items = await request.json();
   return items;
 }
 
 export async function getLatestItems() {
   const items = await getItems();
+
   return items.slice(0, 3);
 }
+// http://localhost:3000/api/items
